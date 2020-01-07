bfa07395e808b0e334c5f1000ecbd8124ef0e702
Michael Schäfer <michael.schaefer@it-structure.de>
2020-01-07 22:47:58 +0100

    Added pg_dump v12

--- build_fhem_main.sh
+++ build_fhem_main.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t computerkoenig/fhem_main:v3.3 --push ./fhem_main
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t computerkoenig/fhem_main:v3.5 --push ./fhem_main
